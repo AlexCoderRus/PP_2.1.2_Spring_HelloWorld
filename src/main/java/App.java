0966d16f2ac70888c562a179cc6ddff3d17e472b
AlexCoderRus <115412380+AlexCoderRus@users.noreply.github.com>
2022-12-18 14:34:48 +0300

    Up1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,11 +8,11 @@ public class App {
         HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
         HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.equals(bean1));
+        System.out.println(bean == bean1);
 
         Cat cat = (Cat) applicationContext.getBean("cat");
         Cat cat1 = (Cat) applicationContext.getBean("cat");
-        System.out.println(cat.equals(cat1));
+        System.out.println(cat == cat1);
 
     }
 }
